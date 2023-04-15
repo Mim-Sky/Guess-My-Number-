c382afd4bbc8b2002577ec25ebfa913be2b3c476
Mim-Sky <horodyski.michal@gmail.com>
2023-04-15 20:12:20 +0000

    refactoring done

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20; // state variable, because this variable is part of app state
 let highscore = 0;
 
+const displayMessage = function(message) {
+  document.querySelector(`.message`).textContent = message;
+}
+
 document.querySelector(`.check`).addEventListener(`click`, function() {
   const guess = Number(document.querySelector(`.guess`).value);
-  
-  
 
   if (!guess) //boolian negation inverter - must be true,     because I want something to happen 
   { // WHEN THERE IS NO INPUT
-    document.querySelector(`.message`).textContent = `No number!`;
+    displayMessage(`No Number!`);
     document.querySelector(`.number`).textContent = secretNumber; 
-    
-  // } else if (score > highscore) {
- 
-
+   
     //WHEN PLAYER WINS
-
+  
   } else if (guess === secretNumber) {
-    document.querySelector(`.message`).textContent = `Correct number!!!`;
+    displayMessage(`Correct number!!!`);
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
       if (score > highscore) {
         highscore = score;
         document.querySelector(`.highscore`).textContent = highscore;
     }
-
-  // WHEN NUMBER IS TOO HIGH
-
-  } else if ( guess > secretNumber) { 
-    if (score > 1) {
-    document.querySelector(`.message`).textContent = `Too high!`; 
-    score--; //(score = score -1;)
-    document.querySelector(`.score`).textContent = score;
-  } else {
-    document.querySelector(`.message`).textContent = `You lost the game!`;
-    document.querySelector(`.score`).textContent = 0;
-  }
-   // WHEN NUMBER IS TOO LOW
-
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(`.message`).textContent = `Too low!`; 
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+  if (score > 1) {
       score--; //(score = score -1;)
+      displayMessage(guess > secretNumber ? `Too high!` : `Too low!`);  
       document.querySelector(`.score`).textContent = score;
     } else {
-      document.querySelector(`.message`).textContent = `You lost the game!`;
+      displayMessage(`You lost the game!`);
       document.querySelector(`.score`).textContent = 0;
     }
   }
 });
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(`.message`).textContent = `Too low!`; 
+  //     score--; //(score = score -1;)
+  //     document.querySelector(`.score`).textContent = score;
+  //   } else {
+  //     document.querySelector(`.message`).textContent = `You lost the game!`;
+  //     document.querySelector(`.score`).textContent = 0;
+  //   }
+  // }
 
 document.querySelector(`.again`).addEventListener(`click`, function(){
   secretNumber = Math.trunc(Math.random() * 20) + 1;  
