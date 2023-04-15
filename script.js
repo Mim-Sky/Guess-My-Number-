fd4619c097758e17d90d7252779306e14b985df3
Mim-Sky <horodyski.michal@gmail.com>
2023-04-15 19:34:49 +0000

    Added highscores functionality

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 'use strict';
 
-// console.log(document.querySelector(`.message`).textContent);
-
-// document.querySelector(`.number`).textContent = 13;
-// document.querySelector(`.score`).textContent = 10;
-// document.querySelector(`.guess`).value = 23;
-// console.log(document.querySelector(`.guess`).value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20; // state variable, because this variable is part of app state
-
-
+let highscore = 0;
 
 document.querySelector(`.check`).addEventListener(`click`, function() {
   const guess = Number(document.querySelector(`.guess`).value);
   
+  
+
   if (!guess) //boolian negation inverter - must be true,     because I want something to happen 
   { // WHEN THERE IS NO INPUT
     document.querySelector(`.message`).textContent = `No number!`;
+    document.querySelector(`.number`).textContent = secretNumber; 
+    
+  // } else if (score > highscore) {
+ 
 
     //WHEN PLAYER WINS
 
@@ -26,7 +24,10 @@ document.querySelector(`.check`).addEventListener(`click`, function() {
     document.querySelector(`.message`).textContent = `Correct number!!!`;
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
-    document.querySelector(`.number`).textContent = secretNumber; 
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector(`.highscore`).textContent = highscore;
+    }
 
   // WHEN NUMBER IS TOO HIGH
 
@@ -54,22 +55,11 @@ document.querySelector(`.check`).addEventListener(`click`, function() {
 });
 
 document.querySelector(`.again`).addEventListener(`click`, function(){
-  // document.querySelector(`.number`).textContent = ;
-  document.querySelector(`body`).style.backgroundColor = `#222`;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;  
   document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.number`).style.width = `15rem`;
-  document.querySelector(`.number`).textContent = `?`;
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector(`.score`).textContent = score;
   document.querySelector(`.guess`).value = ``;
-}
-
-);
-
-
-
-
-
-
-
+  document.querySelector(`.score`).textContent = 20;
+  score = 20;
+});
